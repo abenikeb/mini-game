eae309fc929b5d8504384c9b0af5a078eb305209
abenikeb <abenikeb79@gmail.com>
2024-11-20 21:02:29 +0300

    my-games-add

---
+++ config/env.php
@@ -0,0 +1,9 @@
+<?php 
+// Configuration Variables Set Here
+$ENV_Variables = array(
+    "baseUrl" => "https://196.188.120.3:38443/apiaccess/payment/gateway",
+    "fabricAppId" => "6f918ce4-b5fd-44d0-8f23-65938b25f485",
+    "appSecret" => "b3e1fe7a14384ce7dbab53d709120e17",
+    "merchantAppId" => "1095040632166406",
+    "merchantCode" =>  "220512" 
+);
---
+++ service/createOrderService.php
@@ -0,0 +1,184 @@
+<?php
+require_once('applyFabricTokenService.php');
+require_once('./utils/tool.php');
+require_once('./config/env.php'); 
+
+class CreateOrderService
+{
+    public $req;
+    public $BASE_URL;
+    public $fabricAppId;
+    public $appSecret;
+    public $merchantAppId;
+    public $merchantCode;
+    public $notify_path;
+
+    function __construct($baseUrl, $req, $fabricAppId, $appSecret, $merchantAppId, $merchantCode)
+    {
+        $this->BASE_URL = $baseUrl;
+        $this->req = $req;
+        $this->fabricAppId = $fabricAppId;
+        $this->appSecret = $appSecret;
+        $this->merchantAppId = $merchantAppId;
+        $this->merchantCode = $merchantCode;
+        $this->notify_path = "http://"  . $_SERVER['SERVER_NAME'];
+    }
+    /**
+     * @Purpose: Creating Order
+     *
+     * @Param: no parameters it takes from the constructor
+     * @Return: rawRequest|String
+     */
+    function createOrder()
+    {
+        // Extract values from the request object
+        $title = $this->req->title;
+        $amount = $this->req->amount;
+
+        // Obtain a fabric token using ApplyFabricToken class
+        $applyFabricTokenResult = new ApplyFabricToken(
+            $this->BASE_URL,
+            $this->fabricAppId,
+            $this->appSecret,
+            $this->merchantAppId
+        );
+
+        $result = json_decode($applyFabricTokenResult->applyFabricToken());
+
+        $fabricToken = $result->token; // now we get a fabric token 
+
+        // Make a request to create an order using the obtained fabric token
+        $createOrderResult = $this->requestCreateOrder($fabricToken, $title, $amount);
+
+        // print_r($createOrderResult) ;
+
+        $prepayId = json_decode($createOrderResult)->biz_content->prepay_id;
+
+        $rawRequest = $this->createRawRequest($prepayId);
+
+        echo $rawRequest;
+    }
+
+    /**
+     * @Purpose: Requests CreateOrder
+     *
+     * @Param: fabricToken|String title|string amount|string
+     * @Return: String | Boolean
+     */
+
+    function requestCreateOrder($fabricToken, $title, $amount)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->BASE_URL . '/payment/v1/merchant/preOrder');
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        // Header parameters
+        $headers = array(
+            "Content-Type: application/json",
+            "X-APP-Key: " . $this->fabricAppId,
+            "Authorization: " . $fabricToken
+        );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        // Body parameters
+        $payload = $this->createRequestObject($title, $amount);
+
+        $data = $payload;
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // for dev environment only
+
+        $server_output = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $server_output;
+    }
+    /**
+     * @Purpose: Creating a new merchantOrderId
+     *
+     * @Param: no parameters
+     * @Return: returns a string format of time (UTC)
+     */
+    function createMerchantOrderId_()
+    {
+        return (string)time();
+    }
+    /**
+     * @Purpose: Creating Request Object
+     *
+     * @Param: title|String and amount|String
+     * @Return: Json encoded string
+     */
+    function createRequestObject($title, $amount)
+    {
+        $req = array(
+            'nonce_str' => createNonceStr(),
+            'method' => 'payment.preorder',
+            'timestamp' => createTimeStamp(),
+            'version' => '1.0',
+            'biz_content' => [],
+        );
+
+        $biz = array(
+            // 'notify_url' => 'https://www.google.com',
+            'notify_url' => $this->notify_path . '/api/payment.php', // set your notify end point
+            'business_type' => 'BuyGoods',
+            'trade_type' => 'InApp',
+            'appid' => $this->merchantAppId,
+            'merch_code' => $this->merchantCode,
+            'merch_order_id' => $this->createMerchantOrderId_(),
+            'title' => $title,
+            'total_amount' => $amount,
+            'trans_currency' => 'ETB',
+            'timeout_express' => '120m',
+            'payee_identifier' => '220311',
+            'payee_identifier_type' => '04',
+            'payee_type' => '5000',
+            // 'redirect_url' => $this->path . '/app/product_list.html'
+        );
+
+        $req['biz_content'] = $biz;
+        $req['sign_type'] = 'SHA256WithRSA';
+
+        $req['sign'] = sign($req);
+
+        return json_encode($req);
+    }
+    /**
+     * @Purpose: Create a rawRequest string for H5 page to start pay
+     *
+     * @Param: prepayId returned from the createRequestObject
+     * @Return: rawRequest|string
+     */
+    function createRawRequest($prepayId)
+    {
+        $rawRequest = '';
+
+        $maps = array(
+            "appid" => $this->merchantAppId,
+            "merch_code" => $this->merchantCode,
+            "nonce_str" => createNonceStr(),
+            "prepay_id" => $prepayId,
+            "timestamp" => createTimeStamp()
+        );
+
+        foreach ($maps as $map => $m) {
+            if ($rawRequest == '') {
+                $rawRequest = $map . '=' . $m;
+            } else {
+                $rawRequest = $rawRequest . '&' . $map . '=' . $m;
+            }
+        }
+        $sign = sign($maps);
+        // order by ascii in array
+        $rawRequest = $rawRequest . '&' . 'sign=' . $sign . '&' . 'sign_type=' . "SHA256WithRSA";
+
+        return $rawRequest;
+    }
+}
